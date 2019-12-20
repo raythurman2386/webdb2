4e71b18bbff37072f9832044d0c19799f5cbe42a
raythurman2386 <raymondthurman5@gmail.com>
2019-12-19 22:26:38 -0500

    some boilerplate, all car get request

--- api/routes/carsRouter.js
+++ api/routes/carsRouter.js
@@ -1,6 +1,34 @@
 const carsRouter = require("express").Router();
 const db = require("../../data/db");
 
+// GET all cars
+const getCars = async (req, res, next) => {
+  try {
+    const cars = db("cars").select();
+    return res.status(200).json(cars)
+  } catch (error) {
+    next(error)
+  }
+}
+
+// GET car by ID
+const getCarById = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+// Create Car
+const addCar = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+}
+
 carsRouter
   .get("/", (req, res) => {
     res.send("API is working")
