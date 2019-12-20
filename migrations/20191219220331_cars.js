5040c06c40a735a6cfa53cfdb7adbad9d3f44d59
raythurman2386 <raymondthurman5@gmail.com>
2019-12-19 22:19:58 -0500

    migration/schema initial creation

---
+++ migrations/20191219220331_cars.js
@@ -0,0 +1,23 @@
+
+exports.up = async function (knex) {
+  await knex.schema.createTable("cars", table => {
+    // unique primary key
+    table.increments("id");
+    // required make
+    table.string("make", 128).notNull();
+    // required model
+    table.string("model", 128).notNull();
+    // require vin
+    table.string("vin", 128).notNull().unique();
+    // required mileage
+    table.integer("mileage").notNull();
+    // trans type
+    table.string("transmissionType", 128);
+    // title status
+    table.string("titleStatus", 128);
+  })
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("cars");
+};
