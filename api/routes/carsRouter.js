749c2873873770c8e87b2220880320064bc2a29d
raythurman2386 <raymondthurman5@gmail.com>
2019-12-19 22:51:23 -0500

    db endpoints properly working MVP/STRETCH

--- api/routes/carsRouter.js
+++ api/routes/carsRouter.js
@@ -4,7 +4,7 @@ const db = require("../../data/db");
 // GET all cars
 const getCars = async (req, res, next) => {
   try {
-    const cars = db("cars")
+    const cars = await db("cars")
     return res.status(200).json(cars)
   } catch (error) {
     next(error)
@@ -14,7 +14,7 @@ const getCars = async (req, res, next) => {
 // GET car by ID
 const getCarById = async (req, res, next) => {
   try {
-    const car = db("cars").where({ id: req.params.id });
+    const car = await db("cars").where({ id: req.params.id });
     return res.status(200).json(car);
   } catch (error) {
     next(error)
@@ -23,8 +23,18 @@ const getCarById = async (req, res, next) => {
 
 // Create Car
 const addCar = async (req, res, next) => {
-  try {
+  const payload = {
+    make: req.body.make,
+    model: req.body.model,
+    vin: req.body.vin,
+    mileage: req.body.mileage,
+    transmissionType: req.body.transmissionType,
+    titleStatus: req.body.titleStatus
+  }
 
+  try {
+    const id = await db("cars").insert(payload)
+    return res.status(201).json(await db("cars").where({ id: id }))
   } catch (error) {
     next(error)
   }
@@ -33,5 +43,6 @@ const addCar = async (req, res, next) => {
 carsRouter
   .get("/", getCars)
   .get("/:id", getCarById)
+  .post("/", addCar)
 
 module.exports = carsRouter;
