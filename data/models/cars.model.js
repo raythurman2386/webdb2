4d920ea19ea2a0227c00fccbc3ef88bf86661a34
raythurman2386 <raymondthurman5@gmail.com>
2020-01-06 18:17:18 -0500

    refactoring to use models for db

---
+++ data/models/cars.model.js
@@ -0,0 +1,17 @@
+function find() {}
+
+function findById(id) {}
+
+function add(user) {}
+
+function update(changes, id) {}
+
+function remove(id) {}
+
+module.exports = {
+  find,
+  findById,
+  add,
+  update,
+  delete,
+};
